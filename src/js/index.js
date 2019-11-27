8eedbfee8da5ae04b442330a43e847149a3e2d87
Ivan Dyankov <ivan.dyankov@yoti.com>
2019-11-27 01:04:02 +0000

    added game logic

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,29 @@ import "core-js/stable";
 import "regenerator-runtime/runtime";
 
 import logger from './logger';
+import { start, reset as resetBoard, simulate} from './rpcGameEngine';
 
 import '../css/index.scss';
 
-logger('it works well!');
+logger('let the games begin!');
+
+//TODO:
+// Make the buttons prettier
+// Add unit tests.
+// I didn't follow TDD for this exercise, because the spec was vague
+// and I wasn't sure how to TDD somethign which I kept chaning.
+document.addEventListener("DOMContentLoaded", function() {
+
+  /* The canvas element */
+  const gameBoard = document.getElementById("scene");
+
+  /* Attach Game Controls */
+  const compVcomp = document.getElementById("compVcomp");
+  compVcomp.onclick = simulate(gameBoard);
+
+  const playerControls = document.querySelectorAll(".gameControls__playerChoice button");
+  playerControls.forEach(button => button.onclick = start(gameBoard));
+
+  const reset = document.getElementById("reset");
+  reset.onclick = resetBoard(gameBoard);
+});
---
+++ src/js/rpcGameEngine.js
@@ -0,0 +1,85 @@
+const ROCK = "✊";
+const PAPER = "🖐️";
+const SCISSORS = "✌";
+const GAME_RESULT_DRAW = "DRAW";
+const PLAYER_WON = (player) => `${player} won!`;
+const GAME_OBJECTS = [ROCK, PAPER, SCISSORS];
+const COMPUTER_PLAYER_ONE = "CPU 1";
+const COMPUTER_PLAYER_TWO = "CPU 2";
+const HUMAN_PLAYER = "YOU";
+
+/**
+ * Returns one of [ROCK, PAPER, SCISSORS].
+ * Exported for unit testing only.
+ */
+export const getRandomMove = () => GAME_OBJECTS[Math.floor(Math.random() * 3)];
+
+/**
+ * This function plays a game for Rock, Paper, scissors.
+ * Exported for unit testing only.
+ * @param {String} player1 the value for player 1
+ * @param {String} player2 the vlaue for player 2
+ * @return {String} tells you who won.
+ */
+export const play = (player1, player2) => {
+  if (player1 === ROCK && player2 === ROCK) return GAME_RESULT_DRAW;
+
+  if (player1 === ROCK && player2 === PAPER) return PLAYER_WON(player2);
+
+  if (player1 === ROCK && player2 === SCISSORS) return PLAYER_WON(player1);
+
+  if (player1 === PAPER && player2 === ROCK) return PLAYER_WON(player1);
+
+  if (player1 === PAPER && player2 === PAPER) return GAME_RESULT_DRAW;
+
+  if (player1 === PAPER && player2 === SCISSORS) return PLAYER_WON(player2);
+
+  if (player1 === SCISSORS && player2 === ROCK) return PLAYER_WON(player2);
+
+  if (player1 === SCISSORS && player2 === PAPER) return PLAYER_WON(player1);
+
+  if (player1 === SCISSORS && player2 === SCISSORS) return GAME_RESULT_DRAW;
+}
+
+const drawScene = (gameBoard, playerOneValue, playerOneName, playerTwoValue, playerTwoName) => {
+  const context = gameBoard.getContext('2d');
+  context.font = "20px Arial";
+  context.fillText(playerOneName, 10, 20);
+  context.fillText(playerTwoName, 130, 20);
+  context.font = "50px Arial";
+  context.fillText(playerOneValue, 10, 90);
+  context.fillText(playerTwoValue, 140, 90);
+  const result = document.getElementById("result");
+  result.innerText = play(playerOneValue, playerTwoValue);
+}
+
+export const reset = (gameBoard) => () => {
+  const context = gameBoard.getContext('2d');
+  context.clearRect(0, 0, gameBoard.width, gameBoard.height);
+};
+
+export const simulate = (gameBoard) => () => {
+  reset(gameBoard)();
+  const playerOne = getRandomMove();
+  const playerTwo = getRandomMove();
+  drawScene(
+    gameBoard,
+    playerOne,
+    COMPUTER_PLAYER_ONE,
+    playerTwo,
+    COMPUTER_PLAYER_TWO
+  );
+};
+
+export const start = (gameBoard) => (evt) => {
+  reset(gameBoard)();
+  const playerOne = getRandomMove();
+  const playerTwo = evt.target.value;
+  drawScene(
+    gameBoard,
+    playerOne,
+    COMPUTER_PLAYER_ONE,
+    playerTwo,
+    HUMAN_PLAYER
+  );
+};
